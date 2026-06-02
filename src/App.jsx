e2b6aa9f62274e3c9ba92b7147f66ec97051a40b
Batoul <batoulawadi.46@gmail.com>
2026-06-02 23:09:30 +0300

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-
 import Navbar from "./components/Navbar"
+
 import Home from "./components/Home"
-import DestinationCard from "./components/DestinationCard"
 import Destinations from "./components/Destinations"
 import Packages from "./components/Packages"
 import Book from "./components/Book"
-import "./App.css"
 
-function App() {
-  return (
-   
+import { Routes, Route } from "react-router-dom"
+
+function App(){
+
+  return(
+
     <div>
-       <Navbar />
-      <Home />
-      <Destinations />
-      <Packages />
-      <Book />
+
+      <Navbar />
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/destinations"
+          element={<Destinations />}
+        />
+
+        <Route
+          path="/packages"
+          element={<Packages />}
+        />
+
+        <Route
+          path="/book"
+          element={<Book />}
+        />
+
+      </Routes>
+
     </div>
   )
 }
 
 export default App
-
-
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,27 @@
-function Navbar(){
-    return(
-        <nav>
-            <a href="#home">Home</a>
-            <a href="#destinations">Destinations</a>
-            <a href="#packages">Packages</a>
-            <a href="#book">Book</a>
-            <a href="mailto:ltc@gmail.com">Contact</a>
-        </nav>
-    )
+import { Link } from "react-router-dom"
 
+function Navbar(){
+
+  return(
+
+    <nav>
+
+      <Link to="/">Home</Link>
+
+      <Link to="/destinations">
+        Destinations
+      </Link>
+
+      <Link to="/packages">
+        Packages
+      </Link>
+
+      <Link to="/book">
+        Book
+      </Link>
+
+    </nav>
+  )
 }
 
 export default Navbar
